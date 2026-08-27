16b15f274e92bb416797a2bb5886871ba4a3a118
A. T. M Neamul <47332296+NeamulHaq@users.noreply.github.com>
2026-08-27 15:16:57 +0600

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@
 var map=L.map('map',{zoomControl:false,minZoom:9,maxZoom:18}).setView([23.79,90.41],11);
 L.control.zoom({position:'bottomright'}).addTo(map);
 L.control.scale({position:'bottomright',imperial:false}).addTo(map);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{   subdomains: 'abcd',   maxZoom: 20,   crossOrigin: 'anonymous',   attribution: '© OpenStreetMap contributors © CARTO',   opacity: 0.6 }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{   subdomains: 'abcd',   maxZoom: 20,   crossOrigin: 'anonymous',   attribution: '© OpenStreetMap contributors © CARTO',   opacity: 0.4 }).addTo(map);
 
 var selectedLayer=null,selectedRoutes=[],selectedStops=[];
 var routeLayerById={};
